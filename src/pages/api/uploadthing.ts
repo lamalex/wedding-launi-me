92b4b3c2445549fe5aa2c8f0497a72b43cc462ab
Alex Launi <dev@launi.me>
2024-08-12 22:28:42 -0400

    tweaks

--- src/pages/api/uploadthing.ts
+++ src/pages/api/uploadthing.ts
@@ -1,11 +1,12 @@
 import { createRouteHandler } from "uploadthing/server";
-
 import { ourFileRouter } from "../../server/uploadthing";
 
 // Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
   config: {
+    logLevel: "trace",
+    uploadthingId: import.meta.env.UPLOADTHING_APP_ID,
     uploadthingSecret: import.meta.env.UPLOADTHING_SECRET,
   },
 });
